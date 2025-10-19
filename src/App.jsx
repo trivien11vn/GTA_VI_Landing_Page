f241a12512a62178c89b32a5511d684c8651cccc
trivien11vn <triviencoderr@gmail.com>
2025-10-19 15:48:43 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+
+const App = () => {
+    return (
+        <main>
+            <div>
+                <h1 className='text-3xl text-pink'>Welcome to the world GTA & GSAP</h1>
+            </div>
+        </main>
+    )
+}
+
+export default App
