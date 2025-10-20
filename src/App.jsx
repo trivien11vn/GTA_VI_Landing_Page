23781a4b466c909304a845705735a8ea8ab24c39
trivien11vn <triviencoderr@gmail.com>
2025-10-20 12:35:45 +0700

    update Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ScrollTrigger } from 'gsap/all'
 import Hero from './sections/Hero'
 import Navbar from './sections/Navbar'
 
-
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
     return (
         <main>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,8 @@
-import CoomingSoon from "./CoomingSoon"
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
-import { useMaskSettings } from "../../constants"
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
+import { useMaskSettings } from '../../constants';
+import ComingSoon from "./ComingSoon"
 
 const Hero = () => {
   const { initialMaskPos, initialMaskSize, maskPos, maskSize } = useMaskSettings();
@@ -11,16 +11,11 @@ const Hero = () => {
     gsap.set('.mask-wrapper', {
       maskPosition: initialMaskPos,
       maskSize: initialMaskSize,
-    })
+    });
 
-    gsap.set('.mask-logo', {
-      marginTop: '-100vh',
-      opacity: 0
-    })
+    gsap.set('.mask-logo', { marginTop: '-100vh', opacity: 0 });
 
-    gsap.set('.entrance-message', {
-      marginTop: '0vh'
-    })
+    gsap.set('.entrance-message', { marginTop: '0vh' });
 
     const tl = gsap.timeline({
       scrollTrigger: {
@@ -31,7 +26,20 @@ const Hero = () => {
         pin: true,
       }
     })
-  })
+
+    tl
+      .to('.fade-out', { opacity: 0, ease: 'power1.inOut' })
+      .to('.scale-out', { scale: 1, ease: 'power1.inOut' })
+      .to('.mask-wrapper', { maskSize, ease: 'power1.inOut' }, '<')
+      .to('.mask-wrapper', { opacity: 0 })
+      .to('.overlay-logo', {
+        opacity: 1, onComplete: () => {
+          gsap.to('.overlay-logo', { opacity: 0 });
+        }
+      }, '<')
+      .to('.entrance-message', { duration: 1, ease: 'power1.inOut', maskImage: 'radial-gradient(circle at 50% 0vh, black 50%, transparent 100%)' }, '<')
+  });
+
   return (
     <section className="hero-section">
       <div className="size-full mask-wrapper">
@@ -42,13 +50,16 @@ const Hero = () => {
           <img src="/images/play.png" alt="play" className="w-7 ml-1" />
         </div>
       </div>
+
       <div>
         <img src="/images/big-hero-text.svg" alt="logo" className="size-full object-cover mask-logo" />
       </div>
+
       <div className="fake-logo-wrapper">
         <img src="/images/big-hero-text.svg" className="overlay-logo" />
       </div>
-      <CoomingSoon />
+
+      <ComingSoon />
     </section>
   )
 }
