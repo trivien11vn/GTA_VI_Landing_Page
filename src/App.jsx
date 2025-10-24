bee6303779c7d65eda922b98f7a9c3e04f63e59e
trivien11vn <triviencoderr@gmail.com>
2025-10-24 15:21:00 +0700

    update Outro component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import SecondVideo from './sections/SecondVideo';
 import Lucia from './sections/Lucia';
 import PostCard from './sections/PostCard';
 import Final from './sections/Final';
+import Outro from './sections/Outro';
 
 gsap.registerPlugin(ScrollTrigger);
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
             <Lucia />
             <PostCard />
             <Final />
+            <Outro />
         </main>
     )
 }
